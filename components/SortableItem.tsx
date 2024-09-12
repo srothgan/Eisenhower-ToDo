494e5eb8d7bf0c9bdd46c5ac48e6fcd292180152
Simon Rothgang <srothgan@uni-muenster.de>
2024-09-12 22:06:18 +0200

    readded drag button

--- components/SortableItem.tsx
+++ components/SortableItem.tsx
@@ -21,13 +21,13 @@ const SortableItem = ({ id, name, note, date,deleteItem }: {
       ref={setNodeRef}
       style={{ transform: CSS.Transform.toString(transform), transition }}
       className="w-full flex items-center justify-between my-2 px-4 py-2 bg-slate-200 h-fit"
-      {...attributes} {...listeners}
     >
       <button type="button" onClick={() => deleteItem(id.toString())}> 
       <FaRegCircle />
     </button>
       {/* Pass id, name, note, and date to Item */}
       <Item id={id} name={name} note={note} date={date} />
+      <button {...attributes} {...listeners}><RxDragHandleDots2/></button>
     </div>
   );
 };
